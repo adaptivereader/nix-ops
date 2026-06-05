dff556f3f800c045eb207721cf4ee5368171dee0
Ben Duggan <benaduggan@gmail.com>
2026-06-05 01:02:31 +0000

    Update pkg ${matrix.attrs.attr}

--- mods/pkgs/supabase-cli-stable.nix
+++ mods/pkgs/supabase-cli-stable.nix
@@ -9,19 +9,19 @@
 
 buildGoModule rec {
   pname = "supabase-cli-stable";
-  version = "2.104.0";
+  version = "2.105.0";
 
   src = fetchFromGitHub {
     owner = "supabase";
     repo = "cli";
     rev = "v${version}";
-    hash = "sha256-CuU5CsvTVMkWcmvC40qjGpkFkR+Vr442FVoXBH9J4vs=";
+    hash = "sha256-L56P2ao1N+M9+b76E4gjHfRVGU2JBKE31VxVaDeQk5E=";
   };
 
   # Upstream moved the Go CLI into a monorepo subdirectory at v2.101.0.
   sourceRoot = "${src.name}/apps/cli-go";
 
-  vendorHash = "sha256-xoZG+qbteOK4/V0HduSp9fCI2RH6IkyFnpBRS6ni70Y=";
+  vendorHash = "sha256-1uzkvu1EcIk3+AVnv3GVCQLUPhCKNPvyFIstJvswET0=";
 
   subPackages = [ "." ];
 
