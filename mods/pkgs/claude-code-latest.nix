010bd30c4769742fe2f883285825e867c05aa391
Ben Duggan <benaduggan@gmail.com>
2026-06-13 01:03:20 +0000

    Update pkg ${matrix.attrs.attr}

--- mods/pkgs/claude-code-latest.nix
+++ mods/pkgs/claude-code-latest.nix
@@ -5,24 +5,24 @@
 }:
 
 let
-  version = "2.1.173";
+  version = "2.1.176";
 
   platformMap = {
     "aarch64-darwin" = {
       npmPlatform = "darwin-arm64";
-      sha256 = "0lqrvx18psnc1cf7p2riyjgngbjxvsdacy1chxijj81s0xccxyzr";
+      sha256 = "0949apdsb5fmbkksysafsmgfai2sllb35xvivwhr992jn8kz1ark";
     };
     "x86_64-darwin" = {
       npmPlatform = "darwin-x64";
-      sha256 = "16ngx2br7lg2wz836day26f22m6aym27mbz207ychrypq88v2i6d";
+      sha256 = "12dm9jyzsfixlihnqc84923nm9zr3d19m2hwc4kvaqyv1s8v3ahq";
     };
     "x86_64-linux" = {
       npmPlatform = "linux-x64";
-      sha256 = "09yf2i90i52rp5bpkdnm4d8w3z8mmd52349r0d0bi9pp934xj9rl";
+      sha256 = "1s8z8wvxh30fw7hxvpb0scm3z80vgkmc75s8ixwfkd1ya67lj8yq";
     };
     "aarch64-linux" = {
       npmPlatform = "linux-arm64";
-      sha256 = "14zd3xrhh42n0wn6s3h5z1mmyk7spqsas8hyjx9y14widj1lj5qd";
+      sha256 = "1i01h0p9l1s5w1yq4z13p8skgj51048xjya7612508wvgc0h33y5";
     };
   };
 
