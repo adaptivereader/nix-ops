7af9633897d9e394c06bb988686d85da7c20002f
Ben Duggan <benaduggan@gmail.com>
2026-09-03 03:22:38 +0000

    Update pkg ${matrix.attrs.attr}

--- mods/pkgs/supabase-cli-stable.nix
+++ mods/pkgs/supabase-cli-stable.nix
@@ -8,24 +8,24 @@
 }:
 
 let
-  version = "2.115.0";
+  version = "2.116.0";
 
   platformMap = {
     "aarch64-darwin" = {
       releaseTarget = "darwin_arm64";
-      hash = "sha256-WyVXTv0KZ5BQcwhXg9o2WXN9I35RN+Ot/hqYWOlPQNw=";
+      hash = "10ss31d23mdibcqhmwpdpvyaxc18kmcjcv0cxjf9hb5hsxah8xcb";
     };
     "x86_64-darwin" = {
       releaseTarget = "darwin_amd64";
-      hash = "sha256-DvEPYzxy6hS+kszSkFrxqSRy38HnUY4E6Ie/ygtFuFE=";
+      hash = "1664szqymd5xaiphv0r7gzylbr4712b1fik226957fig49kcw78y";
     };
     "x86_64-linux" = {
       releaseTarget = "linux_amd64";
-      hash = "sha256-/wmWCM51i2JVMu8Dph9MlSC5lelP9s1UgNwEKMrWTLM=";
+      hash = "0n6irxdda1qvp39j3hiffbn60i15ab4f9162widv4lbx575k2c2v";
     };
     "aarch64-linux" = {
       releaseTarget = "linux_arm64";
-      hash = "sha256-AtLf3fQfttA9Lxuvbgxjsy7MjE393L5j+bEa7NKpERw=";
+      hash = "0cvr534d2bpc9a1da2vxrmb1khdd0l304jxllhb9fidqddslanh1";
     };
   };
 
